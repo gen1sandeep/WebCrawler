fa233a34ee10da5a0f53bb230a6fa1a438933571
Sandeep Kumar Reddy Alla <Sandeep_Alla@ad.infosys.com>
2017-06-29 00:14:58 +0530

    Base Skeleton for WebCrawler Maven Project

---
+++ src/main/java/com/pramathi/demo/webcrawler/WebCrawlerApp.java
@@ -0,0 +1,13 @@
+package com.pramathi.demo.webcrawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class WebCrawlerApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
